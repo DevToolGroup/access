d158f040d8992276c8146196f79b71cdca262d48
GroupDevTool <haveabigdream@126.com>
2024-07-05 15:34:28 +0800

    增加GPL-3.0-License

--- engine/src/main/java/group/devtool/access/engine/OperationProvider.java
+++ engine/src/main/java/group/devtool/access/engine/OperationProvider.java
@@ -1,3 +1,11 @@
+/*
+ * The Access Access Control Engine, 
+ * unlike the RBAC model that utilizes static data to implement access control, 
+ * realizes API access control by evaluating if dynamic data satisfies the access control rules.
+ *
+ * License: GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007
+ * See the license.txt file in the root directory or see <http://www.gnu.org/licenses/>.
+ */
 package group.devtool.access.engine;
 
 public enum OperationProvider {
